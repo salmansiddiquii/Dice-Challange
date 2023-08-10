abc5cc91f406253d024e4c2d250d238eb5075113
SALMAN SIDDIQUI <131545147+salmansiddiquii@users.noreply.github.com>
2023-08-10 08:07:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var randomNumber2 =  Math.floor(Math.random() * 6) + 1;
 
 
 
-var image1 = "images/dice"+randomNumber1+".png";
-var image2 = "images/dice"+randomNumber2+".png";
+var image1 = "dice"+randomNumber1+".png";
+var image2 = "dice"+randomNumber2+".png";
 document.querySelector(".img1").setAttribute("src" ,  image1);
 document.querySelector(".img2").setAttribute("src" , image2);
 
